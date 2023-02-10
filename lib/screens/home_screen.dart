748f1bdfbf2ad0a18d9dea16e9ee3c0570debd97
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-02-10 16:21:38 -0300

    temp files and folders

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  static const String routerName = 'Home';
+  const HomeScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('HomeScreen'),
+     ),
+   );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+
+export 'package:preferences_demo_app/screens/settings_screen.dart';
+export 'package:preferences_demo_app/screens/home_screen.dart';
+
